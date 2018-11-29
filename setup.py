148a25ab245334cf3de01d6a81c6bcf49ffcfd6c
Cariad Eccleston <cariad@cariad.me>
2018-11-29 14:10:09 +0000

    Add setup

---
+++ setup.py
@@ -0,0 +1,25 @@
+"""
+"wp-configr" package setup.
+"""
+
+from setuptools import setup
+
+setup(
+    name='wp-configr',
+    version='0.1',
+    description='Applies updates to WordPress wp-config.php files.',
+    url='https://github.com/cariad/wp-configr',
+    author='Cariad Eccleston',
+    author_email='cariad@cariad.me',
+    license='MIT',
+    packages=[
+        'wp_configr'
+    ],
+    install_requires=[
+    ],
+    extras_require={
+        'dev': [
+            'coverage'
+        ]
+    }
+)
