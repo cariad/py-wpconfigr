9c455cc0370823939c362215c69b102322ec2a84
Cariad Eccleston <cariad@cariad.me>
2018-11-29 17:04:52 +0000

    Update description

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup
 
 setup(
     name='wp-configr',
-    version='0.2',
-    description='Applies updates to WordPress wp-config.php files.',
+    version='0.3',
+    description='Read and write properties in WordPress wp-config.php files.',
     url='https://github.com/cariad/wp-configr',
     author='Cariad Eccleston',
     author_email='cariad@cariad.me',
